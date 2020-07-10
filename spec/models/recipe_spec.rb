da3081c886458fa33555ee58507e32d3e7531d38
Dave Winniford <dwinniford@gmail.com>
2020-07-10 16:28:29 -0400

    adds recipe model test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Recipe, type: :model do
   context "creates a recipe and saves to the database" do
     it "saves a recipe with attributes" do
-      recipe = Recipe.create(label: "yummy chicken", source: "serious eats")
+      plan = Plan.create!(title: "Unhealthy", description: "doesn't check boxes")
+      recipe = plan.recipes.build(label: "yummy chicken", source: "serious eats")
+      recipe.save 
       expect(Recipe.find_by(label: "yummy chicken")).to eq(recipe)
     end
     
